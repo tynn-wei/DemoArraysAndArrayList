e4e4a8ce40ba1b48fff7dcdcbc52750e898aec58
20019616 <20019616@rp.edu.sg>
2021-06-03 14:14:34 +0800

    WS commit

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayActivity extends AppCompatActivity {
 
     TextView tv;
@@ -17,6 +19,31 @@ public class ArrayActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView1);
 
         //continue view the code in the worksheet
+
+        String [] fruits = new String [3];
+
+        fruits[0] = "apple";
+        fruits[1] = "banana";
+        fruits[2] = "cherry";
+
+        //delete
+        fruits[1] = null;
+
+        //update
+        fruits[2] = "durian";
+
+        //add
+        fruits[1] = "mango";
+
+        System.out.println("Size is: " + fruits.length);
+
+        String msg = "";
+        for (int i = 0; i < fruits.length; i++) {
+            msg += fruits[i] + "\n";
+        }
+
+        System.out.println(msg);
+        tv.setText(msg);
 
 
 
--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
@@ -17,6 +20,31 @@ public class ArrayListActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView2);
 
         //continue view the code in the worksheet
+
+        ArrayList<String> al = new ArrayList<>();
+
+        al.add("apple");
+        al.add("banana");
+        al.add("cherry");
+
+        //delete
+        al.remove("cherry");
+
+        //update
+        al.set(1, "dragon fruit");
+
+        //add
+        al.add("durian");
+
+        System.out.println("Size is: " + al.size());
+
+        String msg = "";
+        for (int i = 0; i < al.size(); i++){
+            msg += al.get(i) + "\n";
+        }
+
+        System.out.println(msg);
+        tv.setText(msg);
 
 
     }
